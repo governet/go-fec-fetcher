75931824e13d7509bf7f6bae127cc8cd663e501d
Dan Budris <d.c.budris@gmail.com>
2021-09-18 17:45:56 -0400

    initial setup of zip file downlaod

--- cmd/fetch/fetch.go
+++ cmd/fetch/fetch.go
@@ -3,12 +3,12 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"log"
-
+	"github.com/governet/go-fec-fetcher/pkg/downloader"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"log"
 )
+
+const fecBulkDataUrlRoot = "https://fec.gov/files/bulk-downloads/2008/cm08.zip"
 
 type fetchDataOptions struct {
 	filename string
@@ -17,49 +17,29 @@ type fetchDataOptions struct {
 var fd = &fetchDataOptions{}
 
 var fetchDataCmd = &cobra.Command{
-	Use:          "cluster",
-	Short:        "Create workload cluster",
-	Long:         "This command is used to create workload clusters",
-	PreRunE:      preRunCreateCluster,
+	Use:          "bulk-data",
+	Short:        "fetch FEC bulk data",
+	Long:         "This command is used to fetch fec bulk data",
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := fd.fetchData(cmd.Context()); err != nil {
-			return fmt.Errorf("failed to create cluster: %v", err)
+			return fmt.Errorf("failed to fetch FEC bulk data: %v", err)
 		}
 		return nil
 	},
 }
 
 func init() {
-	fetchCmd.AddCommand(fetchDataCmd)
-	fetchDataCmd.Flags().StringVarP(&fd.filename, "filename", "f", "", "Filename that contains EKS-A cluster configuration")
+	rootCmd.AddCommand(fetchDataCmd)
+	fetchDataCmd.Flags().StringVarP(&fd.filename, "filename", "f", "", "Filename to save bulk data to")
 	err := fetchDataCmd.MarkFlagRequired("filename")
 	if err != nil {
 		log.Fatalf("Error marking flag as required: %v", err)
 	}
 }
 
-func preRunCreateCluster(cmd *cobra.Command, args []string) error {
-	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		err := viper.BindPFlag(flag.Name, flag)
-		if err != nil {
-			log.Fatalf("Error initializing flags: %v", err)
-		}
-	})
-	return nil
-}
-
 func (fd *fetchDataOptions) fetchData(ctx context.Context) error {
 	fmt.Println("THIS IS ANNOYING")
+	downloader.DownloadFile(fecBulkDataUrlRoot, fd.filename)
 	return nil
-}
-
-var fetchCmd = &cobra.Command{
-	Use:   "fetch",
-	Short: "fetch resources",
-	Long:  "fetch resources",
-}
-
-func init() {
-	rootCmd.AddCommand(fetchCmd)
 }
--- pkg/downloader/downloader.go
+++ pkg/downloader/downloader.go
@@ -1,1 +1,37 @@
 package downloader
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func DownloadFile(sourceUrl string, destinationFile string) {
+	resp, err := http.Get(sourceUrl)
+	if err != nil {
+		fmt.Printf("error when GETing data: %s", err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+	fmt.Println("status", resp.Status)
+	if resp.StatusCode != 200 {
+		return
+	}
+
+	// Create the file
+	out, err := os.Create(destinationFile)
+	if err != nil {
+		fmt.Printf("error when creating output file: %s", err)
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	fmt.Printf("erro when writing body to output file: %s", err)
+}
